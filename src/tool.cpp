296a9884d615487facb5cee3f6bd1ad322f5ab06
Johann Klähn <johann@jklaehn.de>
2020-06-14 13:53:35 +0200

    Infer resource dir in tool

--- src/tool.cpp
+++ src/tool.cpp
@@ -1,10 +1,15 @@
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/Decl.h>
+#include <clang/Basic/Version.inc>
+#include <clang/Driver/Driver.h>
 #include <clang/Frontend/CompilerInstance.h>
+#include <clang/Tooling/ArgumentsAdjusters.h>
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
 #include <llvm/ADT/StringRef.h>
 #include <llvm/Support/CommandLine.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Program.h>
 
 #include <memory>
 
@@ -26,6 +31,38 @@ public:
   }
 };
 
+std::string findClangExecutable() {
+#define THE_VERSION_SPECIFIC_PATH(version) "clang-" #version
+  for (const char *name :
+       {THE_VERSION_SPECIFIC_PATH(CLANG_VERSION_MAJOR), "clang"}) {
+#undef THE_VERSION_SPECIFIC_PATH
+    if (llvm::ErrorOr<std::string> path = llvm::sys::findProgramByName(name)) {
+      llvm::SmallString<128> resolved;
+      if (llvm::sys::fs::real_path(*path, resolved)) {
+        // Failed to resolve path.
+        resolved = *path;
+      }
+      return resolved.str();
+    }
+  }
+  return {};
+}
+
+std::string findResourceDir() {
+  // CompilerInvocation::GetResourcesPath() assumes that the tool is installed
+  // alongside clang, so it cannot be used here.  Instead, assume that the
+  // "clang" program matches the library that genpybind is compiled against.
+  // FIXME: This is likely wrong, if there are several installed versions.
+  std::string clang_path = findClangExecutable();
+  if (clang_path.empty())
+    return {};
+
+  // Unfortunately CLANG_RESOURCE_DIR is not exposed via the library,
+  // so it cannot be passed in here.  An alternative would be to construct a
+  // driver and use the resource path it deduces.
+  return clang::driver::Driver::GetResourcesPath(clang_path);
+}
+
 } // namespace
 
 int main(int argc, const char **argv) {
@@ -39,6 +76,26 @@ int main(int argc, const char **argv) {
 
   ClangTool tool(options_parser.getCompilations(),
                  options_parser.getSourcePathList());
+
+  tool.appendArgumentsAdjuster(getClangStripOutputAdjuster());
+  tool.appendArgumentsAdjuster(getClangStripDependencyFileAdjuster());
+  tool.appendArgumentsAdjuster(getClangStripSerializeDiagnosticAdjuster());
+  tool.appendArgumentsAdjuster(getClangSyntaxOnlyAdjuster());
+  tool.appendArgumentsAdjuster([](const CommandLineArguments &arguments,
+                                  llvm::StringRef) -> CommandLineArguments {
+    // Return if there already is a -resource-dir or -resource-dir=<arg> flag
+    for (llvm::StringRef arg : arguments) {
+      if (arg.consume_front("-resource-dir") && (arg.empty() || arg[0] == '='))
+        return arguments;
+    }
+    CommandLineArguments result{arguments};
+    std::string resource_dir = findResourceDir();
+    if (!resource_dir.empty())
+      result.push_back("-resource-dir=" + resource_dir);
+    return result;
+  });
+  // TODO: Also append -xc++, -std=c++17, -D__GENPYBIND__?
+  tool.appendArgumentsAdjuster(getInsertArgumentAdjuster("-Wno-everything"));
 
   auto factory = newFrontendActionFactory<GenpybindAction>();
 
