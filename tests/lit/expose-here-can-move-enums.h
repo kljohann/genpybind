1922d55d6fc9cb10347029c4042ac3e9893c51aa
Johann Klähn <johann@jklaehn.de>
2020-07-17 22:03:11 +0200

    Add test for 'expose_here' on enums

---
+++ tests/lit/expose-here-can-move-enums.h
@@ -0,0 +1,23 @@
+// RUN: genpybind-tool -dump-graph=pruned %s -- -std=c++17 -xc++ -D__GENPYBIND__ 2>&1 | FileCheck %s
+#pragma once
+
+#include "genpybind.h"
+
+namespace nested {
+enum class ScopedEnum {
+};
+enum UnscopedEnum {
+};
+}  // namespace nested
+
+struct GENPYBIND(visible) Outer {
+  using Unscoped GENPYBIND(expose_here) = nested::UnscopedEnum;
+  using Scoped GENPYBIND(expose_here) = nested::ScopedEnum;
+};
+
+// CHECK:      Declaration context graph after pruning:
+// TODO: The namespace should have been pruned
+// CHECK-NEXT: |-Namespace 'nested': hidden
+// CHECK-NEXT: `-CXXRecord 'Outer': visible
+// CHECK-NEXT:   |-Enum 'nested::UnscopedEnum' as 'Unscoped': visible
+// CHECK-NEXT:   `-Enum 'nested::ScopedEnum' as 'Scoped': visible
