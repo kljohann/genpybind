f1cde32b8771195b3bcbe2180265a56db0a56034
Johann Klähn <johann@jklaehn.de>
2020-07-21 21:52:48 +0200

    Add test for template instantiation nested within other instantiation

---
+++ tests/lit/explicit-template-instantiations-can-be-nested-in-template-instantiation.h
@@ -0,0 +1,59 @@
+// RUN: genpybind-tool -dump-graph=visibility -dump-graph=pruned %s -- -std=c++17 -xc++ -D__GENPYBIND__ 2>&1 | FileCheck %s
+#pragma once
+
+#include "genpybind.h"
+
+template <typename T>
+struct GENPYBIND(visible) Outer {
+  template <typename V> struct Inner {
+    enum class Whatever { A, B, C };
+  };
+};
+
+template struct GENPYBIND(visible) Outer<void>::Inner<bool>;
+
+template struct Outer<int>;
+template struct GENPYBIND(visible) Outer<int>::Inner<bool>;
+
+extern template struct GENPYBIND(visible) Outer<void>::Inner<float>;
+
+using InstantiationViaAlias GENPYBIND(expose_here) = Outer<bool>::Inner<int>;
+
+extern template struct Outer<double>::Inner<void>;
+using InstantiationOfDeclaredViaAlias GENPYBIND(expose_here) = Outer<double>::Inner<void>;
+
+struct GENPYBIND(visible) Stop {};
+
+// CHECK:      Declaration context graph (unpruned) with visibility of all nodes:
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<bool>::Inner<int>' as 'InstantiationViaAlias': visible
+// CHECK-NEXT: | `-Enum 'Outer<bool>::Inner<int>::Whatever': visible
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<double>::Inner<void>' as 'InstantiationOfDeclaredViaAlias': visible
+// CHECK-NEXT: | `-Enum 'Outer<double>::Inner<void>::Whatever': visible
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<int>' as 'Outer_int_': visible
+// CHECK-NEXT: | `-ClassTemplateSpecialization 'Outer<int>::Inner<bool>' as 'Inner_bool_': visible
+// CHECK-NEXT: |   `-Enum 'Outer<int>::Inner<bool>::Whatever': visible
+// CHECK-NEXT: |-CXXRecord 'Stop': visible
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<void>' as 'Outer_void_': visible
+// CHECK-NEXT: | |-ClassTemplateSpecialization 'Outer<void>::Inner<bool>' as 'Inner_bool_': visible
+// CHECK-NEXT: | | `-Enum 'Outer<void>::Inner<bool>::Whatever': visible
+// CHECK-NEXT: | `-ClassTemplateSpecialization 'Outer<void>::Inner<float>' as 'Inner_float_': visible
+// CHECK-NEXT: |   `-Enum 'Outer<void>::Inner<float>::Whatever': visible
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<bool>' as 'Outer_bool_': visible
+// CHECK-NEXT: `-ClassTemplateSpecialization 'Outer<double>' as 'Outer_double_': visible
+
+// CHECK:      Declaration context graph after pruning:
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<bool>::Inner<int>' as 'InstantiationViaAlias': visible
+// CHECK-NEXT: | `-Enum 'Outer<bool>::Inner<int>::Whatever': visible
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<double>::Inner<void>' as 'InstantiationOfDeclaredViaAlias': visible
+// CHECK-NEXT: | `-Enum 'Outer<double>::Inner<void>::Whatever': visible
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<int>' as 'Outer_int_': visible
+// CHECK-NEXT: | `-ClassTemplateSpecialization 'Outer<int>::Inner<bool>' as 'Inner_bool_': visible
+// CHECK-NEXT: |   `-Enum 'Outer<int>::Inner<bool>::Whatever': visible
+// CHECK-NEXT: |-CXXRecord 'Stop': visible
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<void>' as 'Outer_void_': visible
+// CHECK-NEXT: | |-ClassTemplateSpecialization 'Outer<void>::Inner<bool>' as 'Inner_bool_': visible
+// CHECK-NEXT: | | `-Enum 'Outer<void>::Inner<bool>::Whatever': visible
+// CHECK-NEXT: | `-ClassTemplateSpecialization 'Outer<void>::Inner<float>' as 'Inner_float_': visible
+// CHECK-NEXT: |   `-Enum 'Outer<void>::Inner<float>::Whatever': visible
+// CHECK-NEXT: |-ClassTemplateSpecialization 'Outer<bool>' as 'Outer_bool_': visible
+// CHECK-NEXT: `-ClassTemplateSpecialization 'Outer<double>' as 'Outer_double_': visible
