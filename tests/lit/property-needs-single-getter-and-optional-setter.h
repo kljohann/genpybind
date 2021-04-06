ec27ad33bd4feecf4f7bb99a95683d6cd08bd859
Johann Klähn <johann@jklaehn.de>
2021-04-06 15:47:58 +0200

    Test that incorrect properties are reported in TU order
    This is now trivially true, since decls are processed in TU order.

--- tests/lit/property-needs-single-getter-and-optional-setter.h
+++ tests/lit/property-needs-single-getter-and-optional-setter.h
@@ -4,26 +4,24 @@
 
 #include "genpybind.h"
 
-// TODO: Diagnostics should be emitted in the right order (-> add line number checks then).
-
 struct GENPYBIND(visible) Example {
   int getter_defined_twice() GENPYBIND(getter_for("getter_twice"));
-  // CHECK-DAG: :7: error: getter already defined for 'getter_twice'
+  // CHECK: setter.h:[[# @LINE + 1]]:7: error: getter already defined for 'getter_twice'
   int getter_defined_twice_2() GENPYBIND(getter_for("getter_twice"));
 
   int setter_defined_twice() GENPYBIND(getter_for("setter_twice"));
   void setter_defined_twice(int value) GENPYBIND(setter_for("setter_twice"));
-  // CHECK-DAG: :8: error: setter already defined for 'setter_twice'
+  // CHECK: setter.h:[[# @LINE + 1]]:8: error: setter already defined for 'setter_twice'
   void setter_defined_twice_2(int value) GENPYBIND(setter_for("setter_twice"));
 
   int both_defined_twice() GENPYBIND(getter_for("both_twice"));
-  // CHECK-DAG: :7: error: getter already defined for 'both_twice'
+  // CHECK: setter.h:[[# @LINE + 1]]:7: error: getter already defined for 'both_twice'
   int both_defined_twice_2() GENPYBIND(getter_for("both_twice"));
   void both_defined_twice(int value) GENPYBIND(setter_for("both_twice"));
-  // CHECK-DAG: :8: error: setter already defined for 'both_twice'
+  // CHECK: setter.h:[[# @LINE + 1]]:8: error: setter already defined for 'both_twice'
   void both_defined_twice_2(int value) GENPYBIND(setter_for("both_twice"));
 
-  // CHECK-DAG: :8: error: No getter for the 'prop' property
+  // CHECK: setter.h:[[# @LINE + 1]]:8: error: No getter for the 'prop' property
   void setter_without_getter(int value) GENPYBIND(setter_for("prop"));
 };
 
