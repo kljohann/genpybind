16bedbe39acfd7863e52a52fb010c01cb807e55f
Johann Klähn <johann@jklaehn.de>
2024-09-24 13:36:08 +0200

    Simplify `getObjectForType()`
    Pybind11 2.6 introduces `pybind11::type::of<T>()` and `pybind11::type`, see
    https://github.com/pybind/pybind11/pull/2364.  We can use neither: `of<T>()`
    throws and `type` cannot represent `None`.

--- runtime/genpybind/runtime.h
+++ runtime/genpybind/runtime.h
@@ -8,20 +8,23 @@
 
 namespace genpybind {
 
+/// A non-throwing variant of `pybind11::type::of<T>()`.
+///
+/// Emits a warning and returns `None` if `T` has not previously
+/// been registered.
 template <typename T> auto getObjectForType() -> ::pybind11::object {
   const ::std::type_info &type_info = typeid(T);
   bool throw_if_missing = false;
-  auto *pybind_info =
-      ::pybind11::detail::get_type_info(type_info, throw_if_missing);
-  if (pybind_info == nullptr) {
+  auto handle =
+      ::pybind11::detail::get_type_handle(type_info, throw_if_missing);
+  if (!handle) {
     std::string name = type_info.name();
     ::pybind11::detail::clean_type_id(name);
     PyErr_WarnFormat(PyExc_Warning, /*stack_level=*/7,
                      "Reference to unknown type '%s'", name.c_str());
-    return pybind11::none();
+    return ::pybind11::none();
   }
-  return ::pybind11::reinterpret_borrow<::pybind11::object>(
-      (PyObject *)pybind_info->type);
+  return ::pybind11::reinterpret_borrow<::pybind11::object>(handle);
 }
 
 template <typename T> std::string string_from_lshift(const T &obj) {
