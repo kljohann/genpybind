867fe445f90ecd1b1ec2376916cf142a8fbab4a1
Johann Klähn <johann@jklaehn.de>
2024-12-30 14:17:22 +0100

    Fix tests for pybind 2.13.6
    See https://github.com/pybind/pybind11/pull/5296, which adds
    `self._pybind11_conduit_v1_()` to all wrapped classes.

--- tests/integration/expose-here-can-move-enums_test.py
+++ tests/integration/expose-here-can-move-enums_test.py
@@ -3,11 +3,12 @@
 # SPDX-License-Identifier: MIT
 
 import expose_here_can_move_enums as m
+from helpers import get_proper_members
 
 
 def test_existence():
     names = {name for name in dir(m) if not name.startswith("__")}
     assert names == {"Outer"}
-    names = {name for name in dir(m.Outer) if not name.startswith("__")}
+    names = {name for name in get_proper_members(m.Outer) if not name.startswith("__")}
     assert names == {"Scoped", "Unscoped", "Y"}
     assert hasattr(m.Outer.Scoped, "X")
